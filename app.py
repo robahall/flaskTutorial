04261040292608a79fca7f778a0c93a29419f602
robahall <robahall2@gmail.com>
2020-06-05 21:22:55 -0400

    Flask app page

--- app.py
+++ app.py
@@ -1,12 +1,34 @@
-from flask import Flask
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+from pathlib import Path
+import pickle
 
 app = Flask(__name__)
-
+model = pickle.load(open('models/ridge.pkl', 'rb'))
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def home():
+    return render_template('index.html')
 
+@app.route('/predict',methods=['POST'])
+def predict():
+
+    int_features = [float(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+
+    output = round(prediction[0], 2)
+
+    return render_template('index.html', prediction_text='House should be $ {}'.format(output))
+
+@app.route('/results',methods=['POST'])
+def results():
+
+    data = request.get_json(force=True)
+    prediction = model.predict([np.array(list(data.values()))])
+
+    output = prediction[0]
+    return jsonify(output)
 
 if __name__ == '__main__':
     app.run()
